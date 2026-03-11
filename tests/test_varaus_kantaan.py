8792f57231b847e376f4f91dadb40614f4075095
Milla Laupiainen <mvlaup@utu.fi>
2026-03-11 16:20:50 +0200

    lisää testejä

--- tests/test_varaus_kantaan.py
+++ tests/test_varaus_kantaan.py
@@ -1,27 +1,49 @@
 import unittest
 import requests
 
-class TestaaVarausKantaan(unittest.TestCase):
-    """Perus testirakenne  http-yhteyksien testaamiseen."""
+"""class TestaaVarausKantaan(unittest.TestCase):
+    #Perus testirakenne  http-yhteyksien testaamiseen.
     def testaa_tiedon_syotto(self):
         payload = {
-            "id": "123",
+            "id": 10,
             "email": "test@mail",
-            "maara": "1",
+            "maara": 1,
             "puh": "2",
+            "muuta" : "jtn",
             "pvm": "1"
         }
 
-        vastaus = requests.post('http://localhost:8000/tilaus', json=payload)
-        assert vastaus.status_code == 200 # add assertion here
+        vastaus = requests.post('http://localhost:8000/tilaus/', json=payload)
+        assert vastaus.status_code == 200 # add assertion here"""
 
 class TestaaSaatavuus(unittest.TestCase):
     
     def testaa_tiedon_saanti(self):
-        
-        vastaus = requests.get('http://localhost:8000/saatavuus')
-        assert vastaus.status_code == 200 # add assertion here
+        testi_pvm = "2026-03-10"
+        vastaus = requests.get(f'http://localhost:8000/saatavuus/{testi_pvm}')
+        assert vastaus.status_code == 200 # add assert here
 
 
+class TestaaEpaonnistuminen(unittest.TestCase):
+
+    def testaa_epaonnistunut_saanti(self):
+            testi_pvm = "2026-03-11"
+            vastaus = requests.get(f'http://localhost:8000/saatavuus/{testi_pvm}')
+            assert vastaus.status_code == 500 # add assert here
+
+class TestaaEpaonnistunutTilaus(unittest.TestCase):
+     
+     def testaa_epaonnistunut_tilaus(self):
+            testi_pvm = "2026-03-11"
+            vastaus = requests.get(f'http://localhost:8000/saatavuus/{testi_pvm}')
+            assert vastaus.status_code == 500 # add assert here
+
+class TestaaMockSaatavuus(unittest.TestCase):
+     
+     def testaa_palautuuko_arvot(self):
+          
+     
 if __name__ == '__main__':
     unittest.main()
+
+
